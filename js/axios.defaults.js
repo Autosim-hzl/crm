a5cf8de4cfef1961f1dc341bcfe9b42ab258b1ae
Autosim-hzl <1185328302@qq.com>
2020-08-28 16:14:38 +0800

    登陆完成

---
+++ js/axios.defaults.js
@@ -0,0 +1,11 @@
+axios.defaults.baseURL = "http://localhost:8888";
+axios.defaults.headers['Content-Type']='application/x-www-form-urlencoded';
+axios.defaults.transformRequest = function(data){
+    if(!data)return data;
+    let result=``;
+    for(let attr in data){
+        if(!data.hasOwnProperty(attr)) break;
+        result +=`&${attr}=${data[attr]}`;
+    }
+    return result.substring(1);
+}
---
+++ js/login.js
@@ -0,0 +1,31 @@
+$(function(){
+    //登录功能
+    $(".submit").click(async function(e){
+        let account = $(".userName").val().trim();
+        let password = $(".userPass").val().trim();
+        if(account === "" || password ===""){
+            alert("账号和密码不能为空~");
+            return;
+        }
+        password = md5(password);
+        console.log(account,password)
+        // axios.post("http://localhost:8888/user/login",{
+        //     account,
+        //     password
+        // }).then(value=>{
+        //     console.log(value)
+        // }).catch(err=>{
+        //     console.log(err);
+        // })
+        let res = await axios.post("/user/login",{account,password})
+        console.log(res.data.code);
+        if(parseInt(res.data.code)=== 0){
+            alert("登陆成功")
+            window.location.href="index.html"
+            return ;
+        }
+       else{
+        alert("用户名或密码错误了")
+       }
+    })
+})
